ef3ce66752264440003810b28bf6780801f40445
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-18 09:55:13 +0200

    all exo and README.md added , exo 1 + 2 + 3 + 4 completed , current exo 5

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char this[] = "\"Programming is like building a multilingual puzzle";
+puts(this);
+return (0);
+}
