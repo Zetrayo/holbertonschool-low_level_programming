b60b5f77390aabb30582f7ee4904fccfea5b8651
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-20 15:02:12 +0200

    monstrosity

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include <unistd.h>
+
 /**
 * main - Entry point
 *
@@ -6,7 +7,30 @@
 */
 int main(void)
 {
-char this[] = "_putchar";
-puts(this);
+char loop;
+loop = '_';
+while (loop != '.')
+{
+write(1, &loop, 1);
+if (loop == '_')
+{loop = 'p'; }
+else if (loop == 'p')
+{loop = 'u'; }
+else if (loop == 'u')
+{loop = 't'; }
+else if (loop == 't')
+{loop = 'c'; }
+else if (loop == 'c')
+{loop = 'h'; }
+else if (loop == 'h')
+{loop = 'a'; }
+else if (loop == 'a')
+{loop = 'r'; }
+else if (loop == 'r')
+{loop = '\n'; }
+else if (loop == '\n')
+{loop = '.'; }
+}
+
 return (0);
 }
