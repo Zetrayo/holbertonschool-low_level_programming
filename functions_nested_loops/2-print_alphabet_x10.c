4b9d64505947c6bfb38cc28b63ea8d1dabc9b8db
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-20 15:52:13 +0200

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+/**
+* notmain2 - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int notmain2(void)
+{
+int print_alphabet10(void);
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 int print_alphabet(void)
 {
 int abc;
@@ -11,5 +12,25 @@ write(1, &abc, 1);
 abc++;
 }
 write(1, &eol, 1);
+return (0);
+}
+
+int print_alphabet10(void)
+{
+int abc, i;
+char eol;
+abc = 97;
+eol = '\n';
+i = 0;
+while (i < 10)
+{
+while (abc < 123)
+{
+write(1, &abc, 1);
+abc++;
+}
+write(1, &eol, 1);
+i++;
+}
 return (0);
 }
