a411ab01fca0f29cd917996435e33c3da43a7109
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-20 15:41:30 +0200

    still ugly 8

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,6 +4,20 @@
 *
 * Return: Always 0 (Success)
 */
+int print_alphabet(void)
+{
+int abc;
+char eol;
+abc = 97;
+eol = '\n';
+while (abc < 123)
+{
+write(1, &abc, 1);
+abc++;
+}
+write(1, &eol, 1);
+return (0);
+}
 
 int main(void)
 {
