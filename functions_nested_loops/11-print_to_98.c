c0a4065331c6d52d1ba735f34134c0c89e58e5eb
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 11:12:59 +0200

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+/**
+* main11 - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main11(void)
+{
+int print_to_98(void);
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -190,14 +190,30 @@ _putchar('\n');
 return (0);
 }
 
-void times_table(void)
-{
-}
-
 int add(int a, int b)
 {
 int yes;
 yes = a + b;
 return (yes);
+return (0);
+}
+
+
+void times_table(int here)
+{
+if (here < 98)
+{
+while (here < 98)
+{
+_putchar(here + '0'),
+}
+}
+else if (here > 98)
+{
+while (here > 98)
+{
+_putchar(here + '0'),
+}
+}
 return (0);
 }
