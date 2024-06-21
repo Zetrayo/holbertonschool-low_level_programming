244df16d2bd6c37c78de5a95ae54cdbe7e20e33b
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 09:23:01 +0200

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int _isalpha(void);
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -47,3 +47,16 @@ return (1);
 }
 return (0);
 }
+
+int _isalpha(int c)
+{
+if (c <= 122 && c >= 97)
+{
+return (1);
+}
+else if (c <= 90 && c >= 65)
+{
+return (1);
+}
+return (0);
+}
