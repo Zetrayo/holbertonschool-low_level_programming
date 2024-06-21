7fd9fd1bd7d229d00b351adca84619c66953e3c5
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 09:55:11 +0200

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+* main6 - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main6(void)
+{
+int _abs(void);
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #include <unistd.h>
 
+
 void _putchar(int r);
 int print_sign(int n);
+int _abs(int);
 
 int print_alphabet(void)
 {
@@ -82,6 +84,20 @@ else if (n > 0)
 {
 write(1, &plus, 1);
 return (1);
+}
+return (0);
+}
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+return (n);
+}
+else if (n >= 0)
+{
+return (n);
 }
 return (0);
 }
