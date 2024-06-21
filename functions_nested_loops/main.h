68bc4c599a47178124a7d9d43dcc9c143ffacaf2
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 15:09:48 +0200

    9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -237,26 +237,32 @@ printf("%d, ", n);
 
 void times_table(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 9; a++)
+int x, y, z, u, d;
+for (x = 0; x <= 9; x++)
 {
-  for (b = 0; b <= 9; b++)
-  {
-    c = a * b;
-    d = c % 10;
-    if (c <= 9)
-    {
-    _putchar(c + '0');
-    }
-    else if (c > 9)
-    {
-    _putchar(d + '0');
-    }
-    _putchar(',');
-    _putchar(' ');
-  }
-  _putchar('\n');
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
+if (z > 9)
+{
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
 }
-
-return;
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
+_putchar('\n');
+}
 }
