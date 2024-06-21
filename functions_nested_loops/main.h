9169b5017ca3bce6a5e930c8310f672993007d72
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 14:50:47 +0200

    9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -243,9 +243,10 @@ for (a = 0; a <= 9; a++)
   for (b = 0; b <= 9; b++)
   {
     _putchar(a + '0');
-    _putchar(b + '0');
+    _putchar(',');
+    _putchar(' ');
   }
-  
+  _putchar('\n');
 }
 
 return;
