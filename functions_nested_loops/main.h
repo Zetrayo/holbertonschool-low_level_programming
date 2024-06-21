fa4b9ae3de9ee13c103f6676fe26e6d714319b3f
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-21 14:37:46 +0200

    9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -241,61 +241,61 @@ int n;
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 0)
+printf("%d, ", n * 0);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 1)
+printf("%d, ", n * 1);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 2)
+printf("%d, ", n * 2);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 3)
+printf("%d, ", n * 3);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 4)
+printf("%d, ", n * 4);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 5)
+printf("%d, ", n * 5);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 6)
+printf("%d, ", n * 6);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 7)
+printf("%d, ", n * 7);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 8)
+printf("%d, ", n * 8);
 n++;
 }
 n = 0;
 while (n < 9)
 {
-printf("%d, ", n * 9)
+printf("%d, ", n * 9);
 n++;
 }
 return;
