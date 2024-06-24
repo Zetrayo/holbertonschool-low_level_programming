1878de811cfc7a79d5bd1d31bdf47fe037e061cd
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-24 15:00:57 +0200

    3 fix 2

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _putchar - Entry point
+ * Description: 'prints one symbol'
+ *
+ * @r:variable
+ * Return: 0 (Success)
+ */
+
+int _putchar(char r)
+{
+	write(1, &r, 1);
+	return (0);
+}
+
+/**
  * print_numbers - Entry point
  * Description: 'prints numbers from 0 to 9'
  *
