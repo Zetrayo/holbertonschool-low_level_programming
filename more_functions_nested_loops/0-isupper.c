5e50a9920da6a72178605582a8f2c2614a688a8b
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-24 11:33:56 +0200

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-* _isupper - Entry point
-*
-* Returns 1 is a is uppercase , else it returns 0
-*/
+ * _isupper - Entry point
+ *
+ * Description: 'returns 1 if c is uppercase , else 0'
+ *
+ * @c: Variable
+ * Return: Always 0 (Success)
+ */
 
 int _isupper(char c)
 {
-if (c <= 90 && c >= 65)
-{
-return (1);
-}
-return (0);
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	return (0);
 }
