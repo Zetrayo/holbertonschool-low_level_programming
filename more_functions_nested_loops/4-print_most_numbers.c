5419e2a90838da0007bc88191804b6185a06f196
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-24 15:14:08 +0200

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Description: 'prints numbers from 0 to 9 but skips 2 and 4'
+ *
+ * Return: 0 (Success)
+ */
+
+int print_most_numbers(void)
+{
+	char a;
+
+	a = 48;
+	while (a < 58)
+	{
+		if (a != 50 || a != 52)
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
