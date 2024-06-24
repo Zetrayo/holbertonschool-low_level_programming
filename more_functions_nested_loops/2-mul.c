a1d97e24273f1dfb36a79f029317522f73f91183
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-24 11:48:27 +0200

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: 'multiplies a with b and returns the result'
+ *
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: the result c (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _isupper(char c);
 int _isdigit(char c);
+int mul(int a, int b);
 
 #endif
