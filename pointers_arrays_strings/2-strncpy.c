ea42afab3d269760675a278481d67b486fea53d2
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-28 09:48:11 +0200

    new 2 fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,15 +15,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c = 0;
 
+	if (n > 0)
+	{
+		n = 25;
+	}
+	
 	while (src[c] != '\0')
 	{
-		if (n == 0)
-		{
-			break;
-		}
 		dest[c] = src[c];
 		c++;
-		n--;
 	}
 	dest[c] = '\0';
 	return (dest);
