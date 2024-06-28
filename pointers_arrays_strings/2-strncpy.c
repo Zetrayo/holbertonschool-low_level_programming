5eb84bc0671f8c8d397f780aaa28376b429ca085
Zetrayo <josefeliciano.com98@gmail.com>
2024-06-28 10:13:13 +0200

    new 2 fix 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,26 +5,40 @@
  * _strncpy - Entry point
  * Description: 'prints a determined number of array elements'
  *
- * @dest: pointer pointing to string
- * @src: pointer pointing to string
+ * @a: pointer pointing to string
+ * @b: pointer pointing to string
  * @n: number of characters to copy
  * Return: 0 (Success)
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *a, char *b, int n)
 {
-	int c = 0;
+	int c = 0, d = n;
 
-	if (n > 0)
+	while (d != 0)
 	{
-		n = 25;
-	}
-	
-	while (src[c] != '\0')
-	{
-		dest[c] = src[c];
+		a++;
 		c++;
+		b++;
+		d--;
 	}
-	dest[c] = '\0';
-	return (dest);
+	while (*b != '\0')
+	{
+		if (n == 0)
+		{
+			break;
+		}
+		*a = *b;
+		b++;
+		a++;
+		c++;
+		n--;
+	}
+	*a = '\0';
+	while (c != 0)
+	{
+		a--;
+		c--;
+	}
+	return (a);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,6 +19,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *a);
 char *_strcat(char *a, char *b);
 char *_strncat(char *a, char *b, int n);
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *a, char *b, int n);
 
 #endif
