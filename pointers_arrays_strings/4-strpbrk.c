2562fb190890ed7120792a256b3276f28a2c18cf
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-01 11:21:32 +0200

    exo 4 the third

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strpbrk - Entry point
+ * Description: 'find the first occurrence of a char'
+ *
+ * @s: pointer pointing to string
+ * @accept: character to find
+ * Return: 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int b = 0, i = 0, c = 0;
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*accept == *s)
+			{
+				return (s);
+			}
+			accept++;
+			i++;
+		}
+		while (i != 0)
+		{
+			accept--;
+			i--;
+		}
+		s++;
+	}
+	return (NULL);
+}
