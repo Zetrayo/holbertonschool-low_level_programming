4f9b56f4434486860ff5977c67bbebb3dcf17f6d
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-01 11:37:46 +0200

    exo 5 the third

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strstr - Entry point
+ * Description: 'find the first occurrence of a char'
+ *
+ * @haystack: pointer pointing to string
+ * @needle: string to find
+ * Return: 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int b = 0, i = 0, c = 0;
+
+	while (*haystack != '\0')
+	{
+		while (*needle == *haystack)
+		{
+			needle++;
+			haystack++;
+			i++;
+			if (*needle == '\0')
+			{
+				while (i != 0)
+				{
+					haystack--;
+					i--;
+				}
+				return (haystack);
+			}
+		}
+		while (i != 0)
+		{
+			needle--;
+			haystack--;
+			i--;
+		}
+		haystack++;
+	}
+	return (NULL);
+}
