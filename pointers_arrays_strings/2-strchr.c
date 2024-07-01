56ad5b96edae39428e26c079fc6f8e28c81b3b43
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-01 10:47:28 +0200

    exo 2 the third

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strchr - Entry point
+ * Description: 'find the first occurrence of a char'
+ *
+ * @s: pointer pointing to string
+ * @c: character to find
+ * Return: 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int b = 0;
+
+	while (*s != '\0' && *s != c)
+	{
+		s++;
+	}
+	if (*s == '\0')
+	{
+		return (NULL);
+	}
+	return (s);
+}
