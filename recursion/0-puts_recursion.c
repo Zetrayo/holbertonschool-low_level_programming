ac2c9a9ae29aa13b05fdbbc134fd3b76d06be21e
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-02 11:36:30 +0200

    Exo 0 fix 4

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -14,9 +14,12 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return (0);
+		key = 1;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(*s);
+	if (key == 0)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(*s);
+	}
 }
