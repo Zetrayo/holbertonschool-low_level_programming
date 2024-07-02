f42e883c5418cf0e32c3fa344ad1c6ec9f26dedb
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-02 09:33:12 +0200

    exo 2 fix 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,6 +12,14 @@
 
 char *_strchr(char *s, char c)
 {
+    if (c == '\0')
+    {
+        while (*s != '\0')
+        {
+            s++;
+        }
+        return (s);
+    }
 	while (*s != '\0' && *s != c)
 	{
 		s++;
