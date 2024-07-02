acbe2be1d4f6c98b76d79b01c87045cb89f29a0e
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-02 14:46:55 +0200

    Exo 4 fix 2

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <unistd.h>
-
+void _putchar(char c);
+int count = 1, key = 3, rem;
 /**
  * _sqrt_recursion - Entry point
  * Description: 'returns the value of x raised to the power of y.'
@@ -11,16 +12,27 @@
 
 int _pow_recursion(int x, int y)
 {
-	int rem = x;
-
+	if (key == 3)
+	{
+		rem = x;
+		key = 0;
+	}
 	if (y < 0)
 	{
 		return (-1);
 	}
-	while (y > 0)
+	if (y > 0)
 	{
 		x = x * rem;
 		y--;
+	}
+	if (y == 1)
+	{
+		key = 1;
+	}
+	if (key == 0)
+	{
+		_pow_recursion(x, y);
 	}
 	return (x);
 }
