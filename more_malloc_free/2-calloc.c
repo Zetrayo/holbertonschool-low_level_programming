6621ea952052c944a2478e318b6564ad01554505
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-09 16:34:08 +0200

    exo 2 fix 1

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,1 +1,28 @@
-void *_calloc(unsigned int nmemb, unsigned int size);
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - Entry point
+ * Description: 'allocates memory for array'
+ * @nmemb: array length
+ * @size: size of things in array
+ * Return: NULL (Fail)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr, a;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	a = sizeof(malloc(nmemb * size)) * 3;
+	while (a != 0)
+	{
+		ptr[a] = 0;
+		a--;
+	}
+	return (ptr);
+}
