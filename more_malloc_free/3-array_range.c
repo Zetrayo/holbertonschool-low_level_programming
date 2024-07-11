325a544ab7c41132ebb27fa97caa67b7f5f1f8d1
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-11 09:53:07 +0200

    exo 3 fix 1

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,1 +1,32 @@
-int *array_range(int min, int max);
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - Entry point
+ * Description: 'creates an array of ints'
+ * @min: smallest int
+ * @max: biggest int
+ * Return: NULL (Fail)
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr, a = 0;
+
+	if (max < min)
+	{
+		return (NULL);
+	}
+	ptr = malloc(max * sizeof(int));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (a <= max)
+	{
+		ptr[a] = min;
+		a++;
+		min++;
+	}
+	return (ptr);
+}
