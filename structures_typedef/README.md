5692c39c7c879de5a33e38049eef69ad4b393f0d
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-11 10:47:16 +0200

    Added structures_typedef folder, exo 1, 2, 4 and 5, README.md and dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,4 @@
+exo 1 = function that initialize a variable of type struct dog
+exo 2 = function that prints a struct dog
+exo 4 = function that creates a new dog.
+exo 5 = function that frees dogs.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+#ifndef DOG_H_
+#define DOG_H_
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif
