8fada4c3fc8723607294e46dfabcc114a8d3d0ea
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-11 16:32:39 +0200

    exo 1 fix 4

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,15 +1,22 @@
 #include "dog.h"
+#include <stdio.h>
+
 
 /**
  * init_dog - Entry point
  * Description: 'initializes dog'
- * @d: dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @doggo: dog
+ * @nam: name of dog
+ * @ag: age of dog
+ * @owne: owner of dog
  */
 
 void init_dog(struct dog *doggo, char *nam, float ag, char *owne)
 {
-	struct dog doggo = {.name = nam, .age = ag, .owner = owne};
+	if (doggo != NULL)
+	{
+		doggo->name = nam;
+		doggo->age = ag;
+		doggo->owner = owne;
+	}
 }
