4743188b21e28e3b631878b933fc46a1727c5663
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-11 16:43:07 +0200

    exo 2 fix 1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Entry point
+ * Description: 'prints dog'
+ * @doggo: dog
+ */
+
+void print_dog(struct dog *doggo)
+{
+	if (doggo != NULL)
+	{
+		printf("%s", doggo->name);
+		printf("%d", doggo->age);
+		printf("%s", doggo->owner);
+	}
+}
