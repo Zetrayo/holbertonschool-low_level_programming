acd67a062d52e6c0a03f3db103c4fa987d76328d
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-14 16:13:51 +0200

    exo 4 fix 1

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *new_dog - Entry point
+ * Description: 'makes a new dog'
+ * @new_dog: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog != NULL)
+	{
+		
+	}
+}
