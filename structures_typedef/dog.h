97592a4015e9026d5631e8d119b7b71b035a2027
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-14 16:05:47 +0200

    dog.h fix 12

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,14 +8,16 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct my_dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t, dog;
+} dog_t;
 
-void init_dog(struct my_dog *doggo, char *nam, float ag, char *owne);
-void print_dog(struct my_dog *doggo);
+void init_dog(struct dog *doggo, char *nam, float ag, char *owne);
+void print_dog(struct dog *doggo);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
