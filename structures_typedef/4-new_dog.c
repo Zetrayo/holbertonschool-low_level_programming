703972a3af0839b29d5fcc3b9d9a77a2b75424ec
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-14 16:20:00 +0200

    exo 4 fix 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,8 +10,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog != NULL)
+	int len_name, len_owner;
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (new_dog->name == NULL)
 	{
-		
+		free(new_dog);
+		return (NULL);
 	}
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->age = age;
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,4 +20,30 @@ void print_dog(struct dog *doggo);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s != '\0')
+	{
+		while (*(s + count) != '\0')
+			count++;
+	}
+	return (count);
+}
+
 #endif
