8d796afbd00a5c80564ccdcacf90061a2aced206
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-14 15:55:01 +0200

    exo 2 fix 6

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,27 +12,20 @@ void print_dog(struct dog *doggo)
 {
 	if (doggo != NULL)
 	{
+		if (doggo->name != NULL)
+		{
+			printf("Name: %s\n", doggo->name);
+		}
 		if (doggo->name == NULL)
 		{
 			printf("Name: (nil)\n");
 		}
-		else if (doggo->name != NULL)
-		{
-			printf("Name: %s\n", doggo->name);
-		}
-		if (doggo->age == 0)
-		{
-			printf("Age: (nil)\n");
-		}
-		else if (doggo->age != 0)
-		{
-			printf("Age: %f\n", doggo->age);
-		}
+		printf("Age: %f\n", doggo->age);
 		if (doggo->owner == NULL)
 		{
 			printf("Owner: (nil)\n");
 		}
-		else if (doggo->owner != NULL)
+		if (doggo->owner != NULL)
 		{
 			printf("Owner: %s\n", doggo->owner);
 		}
