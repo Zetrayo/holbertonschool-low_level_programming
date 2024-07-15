e65a123cd55a90bc94b16e8446594c4659dfbfab
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-15 15:09:12 +0200

    function_pointers exo 1 fix 2 , exo 0 fix 4

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int a = 0;
 
+	if (array == NULL || action == NULL || size == 0)
+	{
+		return;
+	}
 	while (size > a)
 	{
 		(*action)(array[a]);
