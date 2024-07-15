89209811798a8e38d09bfda43c9f70eb161989f5
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-15 14:51:30 +0200

    function_pointers exo 0 fix 3

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,43 +1,5 @@
 #include <stdio.h>
 
-void print_name(char *name, void (*f)(char *));
-/**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_as_is(char *name)
-{
-	printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-	unsigned int i;
-
-	printf("Hello, my uppercase name is ");
-	i = 0;
-	while (name[i])
-	{
-		if (name[i] >= 'a' && name[i] <= 'z')
-		{
-			putchar(name[i] + 'A' - 'a');
-		}
-		else
-		{
-			putchar(name[i]);
-		}
-		i++;
-	}
-}
-
 /**
  * print_name - prints the name
  * @name: name to print
