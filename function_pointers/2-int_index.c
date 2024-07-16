664ba4d8f04dc8e087b7d701b23b817c07e00bff
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-16 09:27:17 +0200

    function_pointers exo 2 fix 1

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * int_index - searches for int
+ * @array: array
+ * @size: size of array
+ * @cmp: function
+ *
+ * Return: Nothing
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a = 0, b = 0;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (NULL);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (a < size)
+	{
+		b = (*cmp)(array[a]);
+		if (b == 1)
+		{
+			return (a);
+		}
+		a++;
+	}
+	return (-1);
+}
