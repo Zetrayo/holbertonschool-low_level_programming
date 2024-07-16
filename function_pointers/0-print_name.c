4a52b197fa7114c8211448180ab33a5ef4f9ad60
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-16 09:21:45 +0200

    function_pointers exo 0 fix 5

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * print_name - prints the name
  * @name: name to print
@@ -9,9 +7,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
 	(*f)(name);
 }
