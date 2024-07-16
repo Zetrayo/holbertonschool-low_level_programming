5008b1623d314fd19ecb1ed4641d164c9db25e29
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-16 09:16:03 +0200

    added function_pointers.h

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#ifndef CALC_H_
+#define CALC_H_
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
