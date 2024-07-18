92abffcc89b7fd6c4cbfaef830730fbf124c09fb
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-18 14:39:47 +0200

    variadic_functions exo 1 fix 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @n: number of parameters
+ * @separator: what to print in between numbers
+ * Return: The sum of its parameters or 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	va_list a;
+
+	va_start(a, n);
+	while (i < n)
+	{
+		printf ("%d", va_arg(a, int));
+		if (i < n - 1)
+		{
+			printf ("%s", *separator);
+		}
+		i++;
+	}
+	va_end(a);
+}
