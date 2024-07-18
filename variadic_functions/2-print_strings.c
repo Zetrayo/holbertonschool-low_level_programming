bc41630a371ac046ca49f1affa6c698c80b82bc1
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-18 14:46:18 +0200

    variadic_functions exo 2 fix 1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings
+ *
+ * @n: number of parameters
+ * @separator: what to print in between numbers
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list a;
+
+	va_start(a, n);
+	while (i < n)
+	{
+		printf("%s", va_arg(a, int));
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(a);
+}
