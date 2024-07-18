aeeb0de692877c4b5d53534573221a01a1f5eea1
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-18 14:42:33 +0200

    variadic_functions exo 1 fix 4

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,13 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(a, n);
 	while (i < n)
 	{
-		printf ("%d", va_arg(a, int));
+		printf("%d", va_arg(a, int));
 		if (i < n - 1 && separator != NULL)
 		{
-			printf ("%s", separator);
+			printf("%s", separator);
 		}
 		i++;
 	}
-	printf ("\n");
+	printf("\n");
 	va_end(a);
 }
