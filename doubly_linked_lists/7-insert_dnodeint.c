d9512eb2c2d4b744f074e89f3f7d8abd77ad23a3
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-04 12:26:12 +0200

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -40,6 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int 
 	{
 		thead = new_node->next;
 		thead->prev = new_node;
-	}	
+	}
+    *head = new_node;
 	return (new_node);
 }
