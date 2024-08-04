3cccd899337bb07cce19bed988864636425a9c9f
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-04 12:00:57 +0200

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,15 +8,17 @@
  * @str: string to be saved in new node, must be duplicated
  * Return: Address of new element or NULL if failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *data)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
-	new_node->str = str;
+	new_node->str = strdup(data);
+	new_node->len = strlen(data);
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (new_node);
