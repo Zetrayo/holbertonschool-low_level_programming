9d737d090bc24e6e1aebeaa81a521d12defe2aee
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-04 10:42:44 +0200

    Main.h fix 2

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -12,9 +12,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 #endif
